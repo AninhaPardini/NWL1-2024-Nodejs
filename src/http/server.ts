ef262769d84c4b8fcd98378f1f72e83aef87539a
AninhaPardini <aninhapardini.work@gmail.com>
2024-02-08 01:41:15 -0300

    feat: primeiros arquivos base

---
+++ src/http/server.ts
@@ -0,0 +1,10 @@
+import fastify from 'fastify';
+import { createPoll } from './routes/create-poll';
+
+const app = fastify();
+app.register(createPoll);
+
+app.listen({ port: 3000 }).then(() => {
+  console.log('Server is running on port 3000');
+});
+
